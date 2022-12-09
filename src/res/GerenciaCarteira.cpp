4df107540045a4cadee1eed27c90336029708123
luk3rr <luk3rr@gmail.com>
2022-12-09 00:28:50 -0300

    organizacao do codigo e lancamento de excecoes

--- src/res/GerenciaCarteira.cpp
+++ src/res/GerenciaCarteira.cpp
@@ -16,6 +16,8 @@ std::shared_ptr<Carteira> GerenciaCarteira::getCarteira(std::string nome) {
     }
 }
 
+// ---------------------------------------- ADD/RM Carteira ------------------------------------------------------------
+
 void GerenciaCarteira::adicionarCarteira(std::string nome, double saldo_inicial) {
     // Carteira
     if (saldo_inicial < 0) {
@@ -50,86 +52,64 @@ void GerenciaCarteira::removerCarteira(std::string nome) {
     this->_carteiras.erase(this->_carteiras.find(nome));
 }
 
+// ---------------------------------------- ADD/RM Receita -------------------------------------------------------------
+
 void GerenciaCarteira::adicionarReceita(std::string carteira, double valor, std::string data, std::string categoria) {
     std::shared_ptr<Carteira> minha_carteira = getCarteira(carteira);
     minha_carteira->adicionarReceita(valor, data, categoria);
-}
-
-void GerenciaCarteira::adicionarDespesa(std::string carteira, double valor, std::string data, std::string categoria) {
-    std::shared_ptr<Carteira> minha_carteira = getCarteira(carteira);
-    minha_carteira->adicionarDespesa(valor, data, categoria);
-}
-
-void GerenciaCarteira::adicionarDespesaCartao(std::string carteira, std::string cartao, double valor, std::string data, 
-                                              std::string categoria) {
-
-    std::shared_ptr<Carteira> minha_carteira = getCarteira(carteira);
-    if (minha_carteira->getSubtipo() == "CarteiraBancaria") {
-        std::shared_ptr<CarteiraBancaria> carteira_bancaria;
-        carteira_bancaria = std::dynamic_pointer_cast<CarteiraBancaria>(minha_carteira);
-        carteira_bancaria->getCartaoDeCredito(cartao)->adicionarDespesa(valor, data, categoria);
-    }
-    else {
-        throw ctrexcp::ContaNaoPermiteCartao(carteira, getCarteira(carteira)->getSubtipo());
-    }
-}
-
-void GerenciaCarteira::adicionarTransferencia(double valor, std::string data, std::string categoria, std::string origem,
-                                           std::string destino) {
-    if (origem == destino) {
-        throw trsexcp::OrigemDestinoIguais(origem);
-    }
-
-    std::shared_ptr<Transferencia> transferencia = std::make_shared<Transferencia>(valor, data, categoria, origem, destino);
-
-    std::shared_ptr<Carteira> carteira_origem = getCarteira(origem);
-    std::shared_ptr<Carteira> carteira_destino = getCarteira(destino);
-
-    carteira_origem->setSaldoAtual(carteira_origem->getSaldoAtual() - valor);
-    carteira_destino->setSaldoAtual(carteira_destino->getSaldoAtual() + valor);
-
-    carteira_origem->adicionarTransacao(transferencia);
-    carteira_destino->adicionarTransacao(transferencia);
-
 }
 
 void GerenciaCarteira::removerReceita(std::string carteira, unsigned id) {
     getCarteira(carteira)->removerReceita(id);
 }
 
+// ---------------------------------------- ADD/RM Despesa -------------------------------------------------------------
+
+void GerenciaCarteira::adicionarDespesa(std::string carteira, double valor, std::string data, std::string categoria) {
+    std::shared_ptr<Carteira> minha_carteira = getCarteira(carteira);
+    minha_carteira->adicionarDespesa(valor, data, categoria);
+}
+
 void GerenciaCarteira::removerDespesa(std::string carteira, unsigned id) {
     getCarteira(carteira)->removerDespesa(id);
 }
 
-void GerenciaCarteira::removerDespesaCartao(std::string carteira, std::string cartao, unsigned id) {
-    if (getCarteira(carteira)->getSubtipo() == "CarteiraBancaria") {
-        std::shared_ptr<CarteiraBancaria> carteira_bancaria;
-        carteira_bancaria = std::dynamic_pointer_cast<CarteiraBancaria>(getCarteira(carteira));
-        carteira_bancaria->getCartaoDeCredito(cartao)->removerDespesa(id);
-    }
+// ------------------------------------- ADD/RM Transferencia ----------------------------------------------------------
+
+void GerenciaCarteira::adicionarTransferencia(double valor, std::string data, std::string categoria, std::string origem,
+                                              std::string destino) {
+    if (valor < 0) throw ctrexcp::ValorInvalido(valor);
+    if (origem == destino) throw trsexcp::OrigemDestinoIguais(origem);
+
+    std::shared_ptr<Carteira> carteira_origem = getCarteira(origem);
+    if (carteira_origem->getSaldoAtual() < valor) throw ctrexcp::SaldoInsuficiente(carteira_origem->getSaldoAtual(), valor);
+
+    std::shared_ptr<Carteira> carteira_destino = getCarteira(destino);
+    std::shared_ptr<Transferencia> transferencia = std::make_shared<Transferencia>(valor, data, categoria, origem, destino);
+
+    carteira_origem->setSaldoAtual(carteira_origem->getSaldoAtual() - valor);
+    carteira_destino->setSaldoAtual(carteira_destino->getSaldoAtual() + valor);
+
+    carteira_origem->adicionarTransacao(transferencia);
+    carteira_destino->adicionarTransacao(transferencia);
 }
 
 void GerenciaCarteira::removerTransferencia(std::string carteira, unsigned id) {
-    std::shared_ptr<Carteira> cart = getCarteira(carteira);
+    std::shared_ptr<Carteira> minha_carteira = getCarteira(carteira);
 
-    if (cart->getTransacoes().find(id) == cart->getTransacoes().end()) {
-        throw trsexcp::TransacaoNaoEncontrada(id);
-    }
-
-    std::shared_ptr<Transacao> transacao = cart->getTransacoes().find(id)->second;
+    std::shared_ptr<Transacao> transacao = minha_carteira->getTransacao(id);
 
     if (transacao->getSubtipo() != "transferencia") {
         throw trsexcp::TipoTransacaoInvalido(transacao->getSubtipo());
     }
 
     std::shared_ptr<Transferencia> transferencia;
-    transferencia = std::dynamic_pointer_cast<Transferencia>(cart->getTransacoes().find(id)->second);
+    transferencia = std::dynamic_pointer_cast<Transferencia>(transacao);
 
     std::shared_ptr<Carteira> carteira_origem = this->getCarteira(transferencia->getOrigem());
     std::shared_ptr<Carteira> carteira_destino = this->getCarteira(transferencia->getDestino());
     
-    // Eh necessario verificar se ha saldo suficiente na carteira de destino,
-    // caso contrario a carteira poderia ficar com saldo negativo
+    // Verificar se há saldo suficiente para ser debitado na carteiira de destino
     if (carteira_destino->getSaldoAtual() < transferencia->getValor()) {
         throw ctrexcp::SaldoInsuficiente(carteira_destino->getSaldoAtual(), transferencia->getValor());
     }
@@ -141,60 +121,97 @@ void GerenciaCarteira::removerTransferencia(std::string carteira, unsigned id) {
     carteira_destino->getTransacoes().erase(id);
 }
 
+// ------------------------------------ ADD/RM Cartao de Credito -------------------------------------------------------
+
 void GerenciaCarteira::adicionarCartao(std::string carteira, std::string nome, std::string numero, std::string CVV,
                                        std::string fechamento, double limite_cartao) {
 
-    if (getCarteira(carteira)->getSubtipo() == "CarteiraBancaria") {
+    std::shared_ptr<Carteira> minha_carteira = getCarteira(carteira);
+    if (minha_carteira->getSubtipo() == "CarteiraBancaria") {
         std::shared_ptr<CarteiraBancaria> carteira_bancaria;
-        carteira_bancaria = std::dynamic_pointer_cast<CarteiraBancaria>(getCarteira(carteira));
+        carteira_bancaria = std::dynamic_pointer_cast<CarteiraBancaria>(minha_carteira);
 
         CartaoDeCredito cartao_de_credito(nome, numero, CVV, fechamento, limite_cartao);
         carteira_bancaria->adicionarCartao(cartao_de_credito);
-
-    } else {
-        throw ctrexcp::ContaNaoPermiteCartao(carteira, getCarteira(carteira)->getSubtipo());
+    } 
+    else {
+        throw ctrexcp::ContaNaoPermiteCartao(carteira, minha_carteira->getSubtipo());
     }
 }
 
 void GerenciaCarteira::removerCartao(std::string carteira, std::string cartao) {
-
-    if (getCarteira(carteira)->getSubtipo() == "CarteiraBancaria") {
+    std::shared_ptr<Carteira> minha_carteira = getCarteira(carteira);
+    if (minha_carteira->getSubtipo() == "CarteiraBancaria") {
         std::shared_ptr<CarteiraBancaria> carteira_bancaria;
-        carteira_bancaria = std::dynamic_pointer_cast<CarteiraBancaria>(getCarteira(carteira));
+        carteira_bancaria = std::dynamic_pointer_cast<CarteiraBancaria>(minha_carteira);
 
         carteira_bancaria->removerCartao(cartao);
     }
     else {
-        throw ctrexcp::ContaNaoPermiteCartao(carteira, getCarteira(carteira)->getSubtipo());
+        throw ctrexcp::ContaNaoPermiteCartao(carteira, minha_carteira->getSubtipo());
     }
 }
 
-void GerenciaCarteira::pagarFaturaCartao(std::string carteira, std::string cartao) {
+// -------------------------------- ADD/RM Despesa Cartao de Credito ---------------------------------------------------
 
-    if (getCarteira(carteira)->getSubtipo() == "CarteiraBancaria") {
+void GerenciaCarteira::adicionarDespesaCartao(std::string carteira, std::string cartao, double valor, std::string data, 
+                                              std::string categoria) {
+
+    std::shared_ptr<Carteira> minha_carteira = getCarteira(carteira);
+    if (minha_carteira->getSubtipo() == "CarteiraBancaria") {
         std::shared_ptr<CarteiraBancaria> carteira_bancaria;
-        carteira_bancaria = std::dynamic_pointer_cast<CarteiraBancaria>(getCarteira(carteira));
+        carteira_bancaria = std::dynamic_pointer_cast<CarteiraBancaria>(minha_carteira);
+
+        carteira_bancaria->getCartaoDeCredito(cartao)->adicionarDespesa(valor, data, categoria);
+    }
+    else {
+        throw ctrexcp::ContaNaoPermiteCartao(carteira, minha_carteira->getSubtipo());
+    }
+}
+
+void GerenciaCarteira::removerDespesaCartao(std::string carteira, std::string cartao, unsigned id) {
+    std::shared_ptr<Carteira> minha_carteira = getCarteira(carteira);
+    if (minha_carteira->getSubtipo() == "CarteiraBancaria") {
+        std::shared_ptr<CarteiraBancaria> carteira_bancaria;
+        carteira_bancaria = std::dynamic_pointer_cast<CarteiraBancaria>(minha_carteira);
+
+        carteira_bancaria->getCartaoDeCredito(cartao)->removerDespesa(id);
+    }
+    else {
+        throw ctrexcp::ContaNaoPermiteCartao(carteira, minha_carteira->getSubtipo());
+    }
+}
+
+// ------------------------- Pagar fatura / listar despeas cartao de credito -------------------------------------------
+
+void GerenciaCarteira::pagarFaturaCartao(std::string carteira, std::string cartao) {
+    std::shared_ptr<Carteira> minha_carteira = getCarteira(carteira);
+    if (minha_carteira->getSubtipo() == "CarteiraBancaria") {
+        std::shared_ptr<CarteiraBancaria> carteira_bancaria;
+        carteira_bancaria = std::dynamic_pointer_cast<CarteiraBancaria>(minha_carteira);
 
         carteira_bancaria->pagarFatura(cartao);
     }
     else {
         // TO DO: Ser possivel pagar fatura do cartao com uma carteira
-        throw ctrexcp::ContaNaoPermiteCartao(carteira, getCarteira(carteira)->getSubtipo());
+        throw ctrexcp::ContaNaoPermiteCartao(carteira, minha_carteira->getSubtipo());
     }
 }
 
 void GerenciaCarteira::listarDespesasCartao(std::string carteira, std::string cartao) {
-
-    if (getCarteira(carteira)->getSubtipo() == "CarteiraBancaria") {
+    std::shared_ptr<Carteira> minha_carteira = getCarteira(carteira);
+    if (minha_carteira->getSubtipo() == "CarteiraBancaria") {
         std::shared_ptr<CarteiraBancaria> carteira_bancaria;
-        carteira_bancaria = std::dynamic_pointer_cast<CarteiraBancaria>(getCarteira(carteira));
+        carteira_bancaria = std::dynamic_pointer_cast<CarteiraBancaria>(minha_carteira);
 
         carteira_bancaria->getCartaoDeCredito(cartao)->listarDespesas();
     }
     else {
-        throw ctrexcp::ContaNaoPermiteCartao(carteira, getCarteira(carteira)->getSubtipo());
+        throw ctrexcp::ContaNaoPermiteCartao(carteira, minha_carteira->getSubtipo());
     }
 }
+
+// ----------------------------------------- Metodos de impressao ------------------------------------------------------
 
 void GerenciaCarteira::imprimirCarteiras() {
     if(!(getCarteiras().empty())) {
