1d6502b5bfc2aa2ced9d1480230b3d311e0aa1f0
luk3rr <luk3rr@gmail.com>
2022-12-10 22:41:36 -0300

    reescrever testes CartaoDeCredito

--- tests/res/TesteCartaoDeCredito.cpp
+++ tests/res/TesteCartaoDeCredito.cpp
@@ -1,39 +1,58 @@
 #include "doctest.h"
 #include "CartaoDeCredito.hpp"
 
-// Se forem criadas outras transacoes antes deste teste o ID nao sera igual a 2
-TEST_CASE("Remover Despesa") {
-    CartaoDeCredito banco1("b1", "1234556781", "001", "10", 100);
-    banco1.adicionarDespesa(10, "10/11/1999", "padaria");
-    banco1.adicionarDespesa(15, "10/11/1999", "padaria");
-    CHECK(banco1.removerDespesa(2) == true);
+// Se houver testes que criem transacoes antes deste o id vai mudar
+TEST_CASE("Limite disponível após remover despesa") {
+    CartaoDeCredito card1("b1", "12345", "001", "10", 100);
+    card1.adicionarDespesa(10, "10/11/1999", "padaria");
+    card1.adicionarDespesa(20, "10/11/1999", "padaria");
+    card1.removerDespesa(2);
+    CHECK(card1.getLimite() - card1.getTotalDespesas() == 90);
 }
 
-TEST_CASE("Teste Construtor Cartão - Exceção Limite Negativo") {
-    CHECK_THROWS_AS(CartaoDeCredito cartao("nome", "101010101010101", 
-        "001", "10/11/1999", -10), cdcexcp::LimiteInvalido);
+TEST_CASE("Criar cartão com limite negativo") {
+    CHECK_THROWS_AS(CartaoDeCredito card1("c1", "12345", "001", "10", -10), ctrexcp::ValorInvalido);
 }
 
-TEST_CASE("Teste alterarLimiteCartao - Caso Base") {
-    CartaoDeCredito cartao("nome", "101010101010101", "001", "10/11/1999", 1);
-    cartao.alterarLimiteCartao(1000);
-    CHECK(cartao.getLimite() == 1000);
+TEST_CASE("Alterar limite") {
+    CartaoDeCredito card1("c1", "12345", "001", "12", 1);
+    card1.alterarLimiteCartao(1000);
+    CHECK(card1.getLimite() == 1000);
 }
 
-TEST_CASE("Teste alterarLimiteCartao - Exceção Limite negativo") {
-    CartaoDeCredito cartao("nome", "101010101010101", "001", "10/11/1999", 1);
-    CHECK_THROWS_AS(cartao.alterarLimiteCartao(-100), cdcexcp::LimiteInvalido);
+TEST_CASE("Alterar limite para valor negativo") {
+    CartaoDeCredito card1("c1", "12345", "001", "12", 1);
+    CHECK_THROWS_AS(card1.alterarLimiteCartao(-1), ctrexcp::ValorInvalido);
 }
 
-TEST_CASE("Soma total do valor das despesas") {
-    CartaoDeCredito banco1("b1", "1234556781", "001", "10", 100);
-    banco1.adicionarDespesa(10, "10/11/1999", "padaria");
-    banco1.adicionarDespesa(15, "10/11/1999", "padaria");
-    banco1.adicionarDespesa(1.5, "10/11/1999", "padaria");
-    CHECK(banco1.getTotalDespesas() == 26.5);
+TEST_CASE("Remover despesa não registrada") {
+    CartaoDeCredito card1("c1", "12345", "001", "10", 100);
+    CHECK_THROWS_AS(card1.removerDespesa(99), trsexcp::TransacaoNaoEncontrada);
 }
 
-TEST_CASE("Teste adicionarDespesa - Exceção Limite Excedido") {
-    CartaoDeCredito banco1("b1", "1234556781", "001", "10", 10);
-    CHECK_THROWS_AS(banco1.adicionarDespesa(11, "10/11/1999", "padaria"), cdcexcp::LimiteExcedido);
+TEST_CASE("Somatorio despesas") {
+    CartaoDeCredito card1("b1", "12345", "001", "10", 100);
+    card1.adicionarDespesa(10, "10/11/1999", "padaria");
+    card1.adicionarDespesa(15, "10/11/1999", "padaria");
+    card1.adicionarDespesa(1.5, "10/11/1999", "padaria");
+    CHECK(card1.getTotalDespesas() == 26.5);
+}
+
+TEST_CASE("Adicionar despesa com valor superior ao limite do cartão") {
+    CartaoDeCredito card1("b1", "12345", "001", "10", 10);
+    CHECK_THROWS_AS(card1.adicionarDespesa(11, "10/11/1999", "padaria"), cdcexcp::LimiteExcedido);
+}
+
+TEST_CASE("Adicionar despesa com valor negativo") {
+    CartaoDeCredito card1("b1", "12345", "001", "10", 10);
+    CHECK_THROWS_AS(card1.adicionarDespesa(-5, "01/12/2001", "padaria"), ctrexcp::ValorInvalido);
+}
+
+TEST_CASE("Pegar lista de despesas") {
+    CartaoDeCredito card1("b1", "12345", "001", "10", 100);
+    card1.adicionarDespesa(13, "10/11/1999", "padaria");
+    card1.adicionarDespesa(15, "10/11/1999", "padaria");
+    card1.adicionarDespesa(1.512, "10/11/1999", "padaria");
+    auto lista = card1.getListaDespesas();
+    CHECK(lista.size() == 3);
 }
